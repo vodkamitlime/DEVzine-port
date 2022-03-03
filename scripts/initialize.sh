6a44622bf102f9865b6913a24dbb9806ad607f5d
Haeun Kang <75682050+vodkamitlime@users.noreply.github.com>
2022-03-04 00:41:51 +0900

    Update initialize.sh

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 cd /home/ubuntu/DEVzine-port/server
+sudo apt-get update
+sudo apt-get upgrade
 sudo apt install npm
 npm install
 npm install pm2@latest -g
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt install redis-server
-sudo mkdir -p /var/log/redis/ 
-sudo touch /var/log/redis/redis-server.log 
-sudo chown redis:redis /var/log/redis/redis-server.log
 sudo apt-get install authbind
 sudo touch /etc/authbind/byport/80
 sudo chown ubuntu /etc/authbind/byport/80
